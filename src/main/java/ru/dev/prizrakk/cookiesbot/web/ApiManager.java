6e8ad8cc4e0e7598a607ca3540a87c40620607e6
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:33:15 +0300

    Update ApiManager.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/web/ApiManager.java
@@ -0,0 +1,53 @@
+package ru.dev.prizrakk.cookiesbot.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import static spark.Spark.*;
+
+import ru.dev.prizrakk.cookiesbot.util.Utils;
+
+public class ApiManager extends Utils {
+    private List<ApiInterface> apis;
+
+    public ApiManager() {
+        this.apis = new ArrayList<>();
+    }
+
+    private boolean apisRegistered = false;
+
+    public void registerApis() {
+        getLogger().debug("===================");
+        getLogger().debug("Загрузка web api");
+        getLogger().debug("===================");
+        if (!apisRegistered) {
+            for (ApiInterface api : this.apis) {
+                switch (api.type()) {
+                    case GET:
+                        get(api.getPath(), (req, res) -> {
+                            api.execute(req, res);
+                            return res.body();
+                        });
+                        break;
+                    case POST:
+                        post(api.getPath(), (req, res) -> {
+                            api.execute(req, res);
+                            return res.body();
+                        });
+                        break;
+                    default:
+                        throw new UnsupportedOperationException("Unsupported HTTP method: " + api.type());
+                }
+                getLogger().debug("API " + api.getName() + " зарегистрирована по пути: " + api.getPath());
+            }
+            notFound((req, res) -> {
+                res.type("application/json");
+                return "{\"message\":\"404 - Not Found\"}";
+            });
+            apisRegistered = true; // Устанавливаем флаг после регистрации API
+        }
+    }
+
+    public void addApi(ApiInterface api) {
+        this.apis.add(api);
+    }
+}
