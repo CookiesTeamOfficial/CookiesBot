e6df95f1f053420bde641f8f35d304a87f66fb4b
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:22:52 +0300

    Update RankCard.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/command/slash/server/RankCard.java
@@ -0,0 +1,178 @@
+package ru.dev.prizrakk.cookiesbot.command.slash.server;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.utils.FileUpload;
+import ru.dev.prizrakk.cookiesbot.command.CommandCategory;
+import ru.dev.prizrakk.cookiesbot.command.ICommand;
+import ru.dev.prizrakk.cookiesbot.command.slash.CommandStatus;
+import ru.dev.prizrakk.cookiesbot.database.Database;
+import ru.dev.prizrakk.cookiesbot.database.DatabaseUtils;
+import ru.dev.prizrakk.cookiesbot.database.ExpVariable;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RankCard implements ICommand {
+    @Override
+    public String getName() {
+        return "rank";
+    }
+
+    @Override
+    public String getDescription() {
+        return "rank this is base";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        List<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.USER, "user", "Позволяет узнать инфу пользователя", false));
+        return options;
+    }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.SERVER;
+    }
+
+    @Override
+    public CommandStatus getStatus() {
+        return CommandStatus.OK;
+    }
+
+    private Database database;
+    private DatabaseUtils databaseUtils;
+
+    public RankCard(Database database) {
+        this.database = database;
+        this.databaseUtils = new DatabaseUtils(database);
+    }
+    ExpVariable expVariable;
+    @Override
+    public void execute(SlashCommandInteractionEvent event) throws SQLException {
+        User user = null;
+        String status = null;
+        if (event.getOption("user") != null) {
+            user = event.getOption("user").getAsUser();
+        } else {
+            user = event.getUser();
+        }
+        expVariable = databaseUtils.getPlayerStatsFromDatabase(user.getId(), event.getGuild().getId());
+        try {
+            String avatarUrl = user.getAvatarUrl(); // URL аватарки пользователя
+            String effectiveName = user.getEffectiveName();
+            String username = user.getName();
+            int level = expVariable.getLevel();
+            int experience = expVariable.getExp();
+            int maxExperience = expVariable.getMaxExp();
+
+            byte[] profileImage = generateProfileImage(avatarUrl, effectiveName, username, level, experience, maxExperience);
+            event.replyFiles(FileUpload.fromData(profileImage, "profile_image.png")).queue();
+        } catch (IOException e) {
+            e.printStackTrace();
+            event.reply("Произошла ошибка при создании изображения").setEphemeral(true).queue();
+        }
+    }
+
+    public static byte[] generateProfileImage(String avatarUrl, String effectiveName, String username, int level, int experience, int maxExperience) throws IOException {
+        int width = 900;
+        int height = 200;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+
+        // Задний фон
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+
+        // Загрузка аватарки
+        BufferedImage avatar = ImageIO.read(new URL(avatarUrl));
+        BufferedImage roundedAvatar = getRoundedImage(avatar, 160);
+        g.drawImage(roundedAvatar, 20, 20, null);
+
+        // Рисование текста
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString(effectiveName, 200, 45);
+
+        // Рисовка ника
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 26));
+        g.drawString(username, 200, 70);
+
+        // Рисование уровня
+        g.setFont(new Font("Arial", Font.BOLD, 22));
+        g.drawString("Уровень: " + level, 200, 140);
+
+        // Рисование опыта
+        g.setFont(new Font("Arial", Font.BOLD, 22));
+        g.drawString("Опыт: " + experience + " / " + maxExperience, 340, 140);
+
+        // Рисование прогресс-бара
+        int progressBarWidth = 600;
+        int progressBarHeight = 24;
+        int progressBarX = 200;
+        int progressBarY = 150;
+        int filledWidth = (int) ((double) experience / maxExperience * progressBarWidth);
+
+        // Фон прогресс-бара
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(progressBarX, progressBarY, progressBarWidth, progressBarHeight);
+
+        // Заполненная часть прогресс-бара
+        g.setColor(Color.GREEN);
+        g.fillRect(progressBarX, progressBarY, filledWidth, progressBarHeight);
+
+        // Обводка прогресс-бара
+        g.setColor(Color.BLACK);
+        g.drawRect(progressBarX, progressBarY, progressBarWidth, progressBarHeight);
+
+        // Завершение работы с графикой
+        g.dispose();
+
+        // Конвертация изображения в байты
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        return baos.toByteArray();
+    }
+
+    private static BufferedImage getRoundedImage(BufferedImage image, int diameter) {
+        BufferedImage mask = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = mask.createGraphics();
+
+        applyQualityRenderingHints(g2);
+        g2.fill(new Ellipse2D.Double(0, 0, diameter, diameter));
+        g2.dispose();
+
+        BufferedImage roundImage = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+        g2 = roundImage.createGraphics();
+        applyQualityRenderingHints(g2);
+        g2.drawImage(image, 0, 0, diameter, diameter, null);
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
+        g2.drawImage(mask, 0, 0, null);
+        g2.dispose();
+
+        return roundImage;
+    }
+
+    private static void applyQualityRenderingHints(Graphics2D g2) {
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+    }
+}
