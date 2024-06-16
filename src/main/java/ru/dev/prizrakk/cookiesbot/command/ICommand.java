2abb59b25e9aa30f9c24d2989679802513ce6067
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:26:43 +0300

    Update ICommand.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/command/ICommand.java
@@ -0,0 +1,20 @@
+package ru.dev.prizrakk.cookiesbot.command;
+
+import java.sql.SQLException;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import ru.dev.prizrakk.cookiesbot.command.slash.CommandStatus;
+
+import java.util.List;
+
+public interface ICommand {
+    String getName();
+
+    String getDescription();
+
+    List<OptionData> getOptions();
+    CommandCategory getCategory();
+    CommandStatus getStatus();
+
+    void execute(final SlashCommandInteractionEvent event) throws SQLException;
+}
