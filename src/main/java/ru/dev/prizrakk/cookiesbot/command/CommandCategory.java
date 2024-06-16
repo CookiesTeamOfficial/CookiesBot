5c5a2189739df2bfec1226275db258d3bf031fe1
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:31:11 +0300

    Update CommandCategory.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/command/CommandCategory.java
@@ -0,0 +1,12 @@
+package ru.dev.prizrakk.cookiesbot.command;
+
+public enum CommandCategory {
+    FUN,
+    ADMINISTRATION,
+    USER,
+    SERVER,
+    SYSTEM,
+    MUSIC,
+    OTHER,
+    PREFIX
+}
