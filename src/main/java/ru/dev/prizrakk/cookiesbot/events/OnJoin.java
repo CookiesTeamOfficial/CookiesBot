d5484886104e621ecb3d0109f482e3017dab3be9
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:24:06 +0300

    Update OnJoin.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/events/OnJoin.java
@@ -0,0 +1,28 @@
+package ru.dev.prizrakk.cookiesbot.events;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ru.dev.prizrakk.cookiesbot.database.Database;
+import ru.dev.prizrakk.cookiesbot.database.DatabaseUtils;
+import ru.dev.prizrakk.cookiesbot.database.ExpVariable;
+import ru.dev.prizrakk.cookiesbot.util.Config;
+
+import java.sql.SQLException;
+
+public class OnJoin extends ListenerAdapter {
+    Database database;
+    DatabaseUtils databaseUtils = new DatabaseUtils(database);
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        ExpVariable expVariable;
+        try {
+            expVariable = databaseUtils.getPlayerStatsFromDatabase(event.getMember().getId(), event.getGuild().getId());
+            expVariable.setExp(expVariable.getExp() + 1);
+            database.updateUserExpStats(expVariable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
