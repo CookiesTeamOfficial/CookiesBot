ecd083cf64f367264b3da610b629927545f47ced
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:23:41 +0300

    Update Play.java

--- src/main/java/ru/dev/prizrakk/cookiesbot/command/slash/music/Play.java
+++ src/main/java/ru/dev/prizrakk/cookiesbot/command/slash/music/Play.java
@@ -1,20 +1,15 @@
-package ru.dev.prizrakk.command.slash.music;
+package ru.dev.prizrakk.cookiesbot.command.slash.music;
 
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.channel.Channel;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import ru.dev.prizrakk.ICommand;
-import ru.dev.prizrakk.lavaplayer.GuildMusicManager;
-import ru.dev.prizrakk.lavaplayer.PlayerManager;
+import ru.dev.prizrakk.cookiesbot.command.CommandCategory;
+import ru.dev.prizrakk.cookiesbot.command.ICommand;
+import ru.dev.prizrakk.cookiesbot.command.slash.CommandStatus;
+import ru.dev.prizrakk.cookiesbot.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import ru.dev.prizrakk.util.Config;
-import ru.dev.prizrakk.util.Values;
-import sun.security.krb5.internal.TGSRep;
+import ru.dev.prizrakk.cookiesbot.util.Config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -38,6 +33,17 @@ public class Play implements ICommand {
         options.add(new OptionData(OptionType.STRING, "name", "Какую музыку будет играть диджей за стойкой?", true));
         return options;
     }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.MUSIC;
+    }
+
+    @Override
+    public CommandStatus getStatus() {
+        return CommandStatus.ERROR;
+    }
+
     Config config = new Config();
 
     @Override
@@ -45,7 +51,7 @@ public class Play implements ICommand {
         Member member = event.getMember();
         GuildVoiceState memberVoiceState = member.getVoiceState();
 
-        if(!memberVoiceState.inAudioChannel()) {
+        if (!memberVoiceState.inAudioChannel()) {
             event.reply("Тебя нет в голосовом канале!").queue();
             return;
         }
@@ -53,10 +59,10 @@ public class Play implements ICommand {
         Member self = event.getGuild().getSelfMember();
         GuildVoiceState selfVoiceState = self.getVoiceState();
 
-        if(!selfVoiceState.inAudioChannel()) {
+        if (!selfVoiceState.inAudioChannel()) {
             event.getGuild().getAudioManager().openAudioConnection(memberVoiceState.getChannel());
         } else {
-            if(selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
+            if (!selfVoiceState.getChannel().equals(memberVoiceState.getChannel())) {
                 event.reply("Тебя нет в голосовом канале со мной вернись!").queue();
                 return;
             }
@@ -68,25 +74,9 @@ public class Play implements ICommand {
         } catch (URISyntaxException e) {
             name = "ytsearch:" + name;
         }
+
         PlayerManager playerManager = PlayerManager.get();
-        playerManager.play(event.getGuild(), name);
         event.deferReply().queue();
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        GuildMusicManager guildMusicManager = PlayerManager.get().getGuildMusicManager(event.getGuild());
-        AudioTrackInfo info = guildMusicManager.getTrackScheduler().getPlayer().getPlayingTrack().getInfo();
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Я играю щас:");
-        embedBuilder.setDescription("**Имя:** `" + info.title + "`");
-        embedBuilder.appendDescription("\n**Автор:** `" + info.author + "`");
-        embedBuilder.appendDescription("\n**Ссылка:  [клик]("+ info.uri +")**");
-        embedBuilder.appendDescription("\n**Репит:** `" + Values.isRepeat + "`");
-        String[] parts = info.uri.split("=");
-        embedBuilder.setThumbnail("http://img.youtube.com/vi/" + parts[1] + "/mqdefault.jpg").toString();
-        embedBuilder.setFooter(config.years_author);
-        event.getHook().sendMessageEmbeds(embedBuilder.build()).queue();
+        playerManager.play(event.getGuild(), name, event);
     }
 }
