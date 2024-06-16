17124a60347e1bb835392497b0ab747468bdf3ce
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:20:47 +0300

    Update WebMain.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/web/WebMain.java
@@ -0,0 +1,28 @@
+package ru.dev.prizrakk.cookiesbot.web;
+
+import ru.dev.prizrakk.cookiesbot.web.guild.GuildInfo;
+import ru.dev.prizrakk.cookiesbot.web.utils.Home;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static spark.Spark.port;
+
+public class WebMain {
+    public static void initialize() {
+        port(1111);
+        //spark.sparkContext().setLogLevel("ERROR");
+
+        // Создание и настройка ApiManager
+        ApiManager apiManager = new ApiManager();
+        Logger.getLogger("org").setLevel(Level.OFF);
+        Logger.getLogger("akka").setLevel(Level.OFF);
+        // Добавление API
+        apiManager.addApi(new Home());
+        apiManager.addApi(new GuildInfo());
+
+        // Регистрация всех API
+        apiManager.registerApis();
+    }
+
+}
