443b1c26fdb60eeecf33275bfa9f41202397c88a
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:25:33 +0300

    Update LoggerManager.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/manager/LoggerManager.java
@@ -0,0 +1,27 @@
+package ru.dev.prizrakk.cookiesbot.manager;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
+
+public class LoggerManager {
+    private final ColorManager color = new ColorManager();
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss ");
+    private final String timestamp = dateFormat.format(new Date());
+    public void info(String message) {
+        System.out.println("[" + timestamp + color.ANSI_GREEN + "INFO" + color.ANSI_RESET + "] " + message);
+        Logger.getLogger("DEBUG").info("sssssssssss");
+    }
+    public void debug(String message) {
+        System.out.println("[" + timestamp + color.ANSI_BLUE + "DEBUG" + color.ANSI_RESET + "] " + message);
+    }
+    public void warn(String message) {
+        System.out.println("[" + timestamp + color.ANSI_YELLOW + "WARN" + color.ANSI_RESET + "] " + message);
+    }
+    public void error(String message) {
+        System.out.println("[" + timestamp + color.ANSI_RED + "ERROR" + color.ANSI_RESET + "] " + message);
+    }
+    public void criticalError(String message) {
+        System.out.println( "[" + timestamp + color.ANSI_RED + "!!!ALERT!!!" + color.ANSI_RESET + "] " + message);
+    }
+}
