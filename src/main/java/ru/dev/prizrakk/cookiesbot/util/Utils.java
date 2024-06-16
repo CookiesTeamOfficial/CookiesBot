9b914719215bce395d4064fdd9f57f1521bc2fde
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:21:28 +0300

    Update Utils.java

---
+++ src/main/java/ru/dev/prizrakk/cookiesbot/util/Utils.java
@@ -0,0 +1,9 @@
+package ru.dev.prizrakk.cookiesbot.util;
+
+import ru.dev.prizrakk.cookiesbot.manager.LoggerManager;
+
+public class Utils {
+    public static LoggerManager getLogger() {
+        return new LoggerManager();
+    }
+}
