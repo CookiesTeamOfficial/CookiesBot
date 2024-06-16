b6960b81c01816a998174eed6148365cfeec0714
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:23:30 +0300

    Update PlayerManager.java

--- src/main/java/ru/dev/prizrakk/cookiesbot/lavaplayer/PlayerManager.java
+++ src/main/java/ru/dev/prizrakk/cookiesbot/lavaplayer/PlayerManager.java
@@ -1,4 +1,4 @@
-package ru.dev.prizrakk.lavaplayer;
+package ru.dev.prizrakk.cookiesbot.lavaplayer;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
@@ -7,7 +7,12 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import ru.dev.prizrakk.cookiesbot.util.Config;
+import ru.dev.prizrakk.cookiesbot.util.Values;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,8 +20,8 @@ import java.util.Map;
 public class PlayerManager {
 
     private static PlayerManager INSTANCE;
-    private Map<Long, GuildMusicManager> guildMusicManagers = new HashMap<>();
-    private AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
+    private final Map<Long, GuildMusicManager> guildMusicManagers = new HashMap<>();
+    private final AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
 
     private PlayerManager() {
         AudioSourceManagers.registerRemoteSources(audioPlayerManager);
@@ -24,7 +29,7 @@ public class PlayerManager {
     }
 
     public static PlayerManager get() {
-        if(INSTANCE == null) {
+        if (INSTANCE == null) {
             INSTANCE = new PlayerManager();
         }
         return INSTANCE;
@@ -33,35 +38,49 @@ public class PlayerManager {
     public GuildMusicManager getGuildMusicManager(Guild guild) {
         return guildMusicManagers.computeIfAbsent(guild.getIdLong(), (guildId) -> {
             GuildMusicManager musicManager = new GuildMusicManager(audioPlayerManager, guild);
-
             guild.getAudioManager().setSendingHandler(musicManager.getAudioForwarder());
-
             return musicManager;
         });
     }
 
-    public void play(Guild guild, String trackURL) {
+    public void play(Guild guild, String trackURL, SlashCommandInteractionEvent event) {
         GuildMusicManager guildMusicManager = getGuildMusicManager(guild);
         audioPlayerManager.loadItemOrdered(guildMusicManager, trackURL, new AudioLoadResultHandler() {
             @Override
             public void trackLoaded(AudioTrack track) {
                 guildMusicManager.getTrackScheduler().queue(track);
+                sendPlayingTrackEmbed(event, track);
             }
 
             @Override
             public void playlistLoaded(AudioPlaylist playlist) {
                 guildMusicManager.getTrackScheduler().queue(playlist.getTracks().get(0));
+                sendPlayingTrackEmbed(event, playlist.getTracks().get(0));
             }
 
             @Override
             public void noMatches() {
-
+                event.getHook().sendMessage("Не удалось найти трек по запросу: " + trackURL).queue();
             }
 
             @Override
             public void loadFailed(FriendlyException exception) {
-
+                event.getHook().sendMessage("Ошибка при загрузке трека: " + exception.getMessage()).queue();
             }
         });
+    }
+
+    private void sendPlayingTrackEmbed(SlashCommandInteractionEvent event, AudioTrack track) {
+        AudioTrackInfo info = track.getInfo();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Я играю щас:");
+        embedBuilder.setDescription("**Имя:** `" + info.title + "`");
+        embedBuilder.appendDescription("\n**Автор:** `" + info.author + "`");
+        embedBuilder.appendDescription("\n**Ссылка:  [клик](" + info.uri + ")**");
+        embedBuilder.appendDescription("\n**Репит:** `" + Values.isRepeat + "`");
+        String[] parts = info.uri.split("=");
+        embedBuilder.setThumbnail("http://img.youtube.com/vi/" + parts[1] + "/mqdefault.jpg").toString();
+        embedBuilder.setFooter(new Config().years_author);
+        event.getHook().sendMessageEmbeds(embedBuilder.build()).queue();
     }
 }
