f9a1a5be5f3bc6d835fdbffbfc3ca7d9fab98e98
Dev_prizrakk <am2005680@gmail.com>
2024-06-17 00:29:59 +0300

    Update ConsoleManager.java

--- src/main/java/ru/dev/prizrakk/cookiesbot/manager/ConsoleManager.java
+++ src/main/java/ru/dev/prizrakk/cookiesbot/manager/ConsoleManager.java
@@ -1,20 +1,13 @@
-package ru.dev.prizrakk.manager;
-
-import net.dv8tion.jda.api.entities.Guild;
+package ru.dev.prizrakk.cookiesbot.manager;
 
 import java.io.Console;
-import java.util.Scanner;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ConsoleManager extends ListenerAdapter
 {
     public JDA jda;
-    LoggerManager log;
-    
-    public ConsoleManager() {
-        this.log = new LoggerManager();
-    }
     Console console = System.console();
     public void console() {
         Console console = System.console();
